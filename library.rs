fb476d396b5412b6bd3664b3471d040ed7fadc40
Stephanie L'Heureux <stephanielh1111@gmail.com>
2023-07-02 21:30:20 -0700

    Completed execrise

--- library.rs
+++ library.rs
@@ -10,10 +10,10 @@ struct Book {
 impl Book {
   // This is a constructor, used below.
   fn new(title: &str, year: u16) -> Book {
-      Book {
-          title: String::from(title),
-          year,
-      }
+    Book {
+      title: String::from(title),
+      year,
+    }
   }
 }
 
@@ -25,28 +25,38 @@ impl Book {
 // - `self` for unique access by value.
 impl Library {
   fn new() -> Library {
-      todo!("Initialize and return a `Library` value")
+    Library {
+      books: Vec::new()
+    }
   }
 
-  //fn len(self) -> usize {
-  //    todo!("Return the length of `self.books`")
-  //}
+  fn len(&self) -> usize {
+    self.books.len()
+  }
 
-  //fn is_empty(self) -> bool {
-  //    todo!("Return `true` if `self.books` is empty")
-  //}
+  fn is_empty(&self) -> bool {
+    self.books.is_empty()
+  }
 
-  //fn add_book(self, book: Book) {
-  //    todo!("Add a new book to `self.books`")
-  //}
+  fn add_book(&mut self, book: Book) -> () {
+    self.books.push(book);
+  }
 
-  //fn print_books(self) {
-  //    todo!("Iterate over `self.books` and each book's title and year")
-  //}
+  fn print_books(&self) -> (){
+    for book in &self.books { 
+      println!("Title: {}    Year: {}", book.title, book.year);
+    }
+  }
 
-  //fn oldest_book(self) -> Option<&Book> {
-  //    todo!("Return a reference to the oldest book (if any)")
-  //}
+  fn oldest_book(&self) -> Option<&Book> {
+    let mut oldest: Option<&Book> = None;
+    for book in self.books.iter() {
+      if oldest.is_none() || book.year < oldest.unwrap().year {
+        oldest = Some(book);
+      }
+    }
+    oldest
+  }
 }
 
 // This shows the desired behavior. Uncomment the code below and
@@ -54,23 +64,23 @@ impl Library {
 // method signatures, including the "self" parameter! You may
 // also need to update the variable bindings within main.
 fn main() {
-  let library = Library::new();
+  let mut library = Library::new();
 
-  //println!("The library is empty: library.is_empty() -> {}", library.is_empty());
-  //
-  //library.add_book(Book::new("Lord of the Rings", 1954));
-  //library.add_book(Book::new("Alice's Adventures in Wonderland", 1865));
-  //
-  //println!("The library is no longer empty: library.is_empty() -> {}", library.is_empty());
-  //
-  //
-  //library.print_books();
-  //
-  //match library.oldest_book() {
-  //    Some(book) => println!("The oldest book is {}", book.title),
-  //    None => println!("The library is empty!"),
-  //}
-  //
-  //println!("The library has {} books", library.len());
-  //library.print_books();
+  println!("The library is empty: library.is_empty() -> {}", library.is_empty());
+
+  library.add_book(Book::new("Lord of the Rings", 1954));
+  library.add_book(Book::new("Alice's Adventures in Wonderland", 1865));
+  
+  println!("The library is no longer empty: library.is_empty() -> {}", library.is_empty());
+  
+  
+  library.print_books();
+  
+  match library.oldest_book() {
+     Some(book) => println!("The oldest book is {}", book.title),
+     None => println!("The library is empty!"),
+  }
+  
+  println!("The library has {} books", library.len());
+  library.print_books();
 } 
